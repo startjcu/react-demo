8fb55048620943f016747e8f3548622b37ce9d37
star <startjcu@163.com>
2019-05-20 16:49:22 +0800

    使用redux-thunk的同时使用redux-devtools中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
 import thunk from 'redux-thunk'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+const store = createStore(reducer, enhancer)
 
 export default store
