b9dac0710001d54bd7a1c7dbc8e4311356d829b1
star <startjcu@163.com>
2019-05-20 15:15:09 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Input, Button, List } from 'antd'
 
-class TodoListUI extends Component {
-    render() {
-        const { inputValue, handleInputChange, handleKeyDown, handleBtnClick, list, handleItemClick } = this.props
-        return (
-            <div style={{ margin: '10px 0 0 10px' }}>
-                <Input
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    onKeyDown={handleKeyDown}
-                    placeholder='todo item'
-                    style={{ width: '300px' }}
-                />
-                <Button
-                    type="primary"
-                    onClick={handleBtnClick}
-                >提交</Button>
-                <List
-                    bordered
-                    dataSource={list}
-                    renderItem={(item, index) => (
-                        <List.Item onClick={() => { handleItemClick(index) }}>{item}</List.Item>)}
-                    style={{ width: '363.84px', marginTop: '5px' }}
-                />
-            </div>
-        )
-    }
+const TodoListUI = (props) => {
+    const { inputValue, handleInputChange, handleKeyDown, handleBtnClick, list, handleItemClick } = props
+    return (
+        <div style={{ margin: '10px 0 0 10px' }}>
+            <Input
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                placeholder='todo item'
+                style={{ width: '300px' }}
+            />
+            <Button
+                type="primary"
+                onClick={handleBtnClick}
+            >提交</Button>
+            <List
+                bordered
+                dataSource={list}
+                renderItem={(item, index) => (
+                    <List.Item onClick={() => { handleItemClick(index) }}>{item}</List.Item>)}
+                style={{ width: '363.84px', marginTop: '5px' }}
+            />
+        </div>
+    )
 }
 
 export default TodoListUI
